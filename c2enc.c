34487ed912ac1aa7730f63528803984c29881b12
Sebastien F4GRX <f4grx@f4grx.net>
2017-06-07 15:47:20 +0200

    fix indentation

--- c2enc.c
+++ c2enc.c
@@ -18,9 +18,9 @@ int c2enc_write(struct c2enc_context_s *ctx, int16_t *buf, uint32_t nsamples)
   uint32_t done = 0;
   while(nsamples >= CODEC2_INPUTSAMPLES)
     {
-    c2enc_process_samples(ctx, buf + done);
-    done += CODEC2_INPUTSAMPLES;
-    nsamples -= CODEC2_INPUTSAMPLES;
+      c2enc_process_samples(ctx, buf + done);
+      done += CODEC2_INPUTSAMPLES;
+      nsamples -= CODEC2_INPUTSAMPLES;
     }
   return done;
 }
